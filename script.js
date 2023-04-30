5bf11d0f82e60f238c7c6faf3af8a5634ceb8c13
fblcak <frederickblcak@gmail.com>
2023-04-30 18:18:40 +0100

    diagnostic

---
+++ script.js
@@ -0,0 +1,16 @@
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap');
+
+ {
+
+  0;
+  0;
+}
+
+do {
+    body
+} while (condition);{
+    font-fado {
+        m
+    } while (condition);ily: 'Comfortaa', cursive;
+    background-color:     white;
+}
